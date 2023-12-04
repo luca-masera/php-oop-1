09d7d31c5c616195d1cbeb2a35fec85400f7e81a
luca-masera <lucamasera1995@gmail.com>
2023-12-04 23:53:17 +0100

    correzioni esercizio e creazioni card film

--- index.php
+++ index.php
@@ -1,24 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
-    <title>Document</title>
-</head>
-
-<body>
-    <?php
-    include __DIR__ . '/Views/header.php';
-    include __DIR__ . '/Views/main.php';
-    include __DIR__ . '/Views/footer.php';
-
-    ?>
-
-
-</body>
-
-</html>
+<?php
+include __DIR__ . '/Views/header.php';
+include __DIR__ . '/Model/Movie.php';
+?>
+<section class="container">
+    <div class="row gy-5 ">
+        <?php foreach ($movies as $movie) {
+            $movie->printCard();
+        } ?>
+    </div>
+</section>
+<?php
+include __DIR__ . 'Views/footer.php';
+?>
